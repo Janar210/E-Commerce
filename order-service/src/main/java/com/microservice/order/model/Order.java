053717171cf1e883f1784eed1b4945bac0793cdf
Janar <99465347+Janar210@users.noreply.github.com>
2026-03-06 12:46:52 +0530

    Add files via upload

---
+++ order-service/src/main/java/com/microservice/order/model/Order.java
@@ -0,0 +1,19 @@
+package com.microservice.order.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private String productId;
+    private int quantity;
+}
