7ae2706b94e73246ed5bc5c9b0785dc23448cbfa
Janar <99465347+Janar210@users.noreply.github.com>
2026-03-06 12:34:14 +0530

    Add files via upload

---
+++ order-service/src/main/java/com/microservice/order/db/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.order.db;
+
+import com.microservice.order.model.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
